5a2bb6a1bc6ec900ff9dae2df8af050e851374d6
Emre-Kul <emrekul35@gmail.com>
2018-07-08 22:35:30 +0300

    init

---
+++ math/matrix4.js
@@ -0,0 +1,59 @@
+const Matrix4 = function () { }
+
+Matrix4.prototype.unit = function () {
+    return [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ]
+}
+
+Matrix4.prototype.multiply = function () { }
+
+Matrix4.prototype.translate = function (vec) {
+    return [
+        [1, 0, 0, vec.x],
+        [0, 1, 0, vec.y],
+        [0, 0, 1, vec.z],
+        [0, 0, 0, 1]
+    ]
+}
+
+Matrix4.prototype.scale = function (vec) {
+    return [
+        [vec.x, 0, 0, 0],
+        [0, vec.y, 0, 0],
+        [0, 0, vec.z, 0],
+        [0, 0, 0, 1]
+    ]
+}
+
+Matrix4.prototype.rotateX = function (angle) {
+    return [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ]
+}
+
+Matrix4.prototype.rotateY = function (angle) {
+    return [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ]
+}
+
+Matrix4.prototype.rotateZ = function (angle) {
+    return [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ]
+}
+
+Matrix4.prototype.rotate = function () { }
---
+++ math/vector3.js
@@ -0,0 +1,10 @@
+
+const Vector3 = function () {
+    let x, y, z;
+}
+
+Vector3.prototype.zero = function(){
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+}
