042daf0901ba8634c33654ffc3ce2a040a597d3e
Emre-Kul <emrekul35@gmail.com>
2018-07-09 21:13:18 +0300

    webpack added

---
+++ index.js
@@ -0,0 +1,5 @@
+const Matrix4 = require('./src/math/matrix4.js');
+const Vector3 = require('./src/math/vector3.js');
+
+const Shader = require('./src/utils/shader.js');
+const WebglUtils = require('./src/utils/webgl-utils.js');
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/
+            }
+        ]
+    }
+};
