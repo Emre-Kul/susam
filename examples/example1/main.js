4ee1bbccefc6307a8ad0fc1878a37fad5f08e548
Emre-Kul <emrekul35@gmail.com>
2018-07-11 22:54:31 +0300

    firs example draft created

---
+++ examples/example1/main.js
@@ -0,0 +1,17 @@
+let gl;
+
+const init = function () {
+    const canvas = document.getElementById('gl-canvas');
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl)
+        alert("Webgl isn't avaliable!");
+        
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+}
+
+window.onload = init;
