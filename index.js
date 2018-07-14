d26bfe8592d360ff6d4d3105d8eac6d956272fcf
Emre-Kul <emrekul35@gmail.com>
2018-07-14 18:15:21 +0300

    shape added to library and first example created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     Matrix4 : require('./src/math/matrix4.js'),
     Vector3 :  require('./src/math/vector3.js'),
-    Shader : require('./src/utils/shader.js')
+    Shader : require('./src/utils/shader.js'),
+    Shape : require('./src/utils/shape.js')
 }
---
+++ src/utils/shape.js
@@ -0,0 +1,35 @@
+
+module.exports = {
+    
+    rect: function (center, width, height) {
+        return [
+            center[0]-width/2,center[1]+height/2,
+            center[0]+width/2,center[1]+height/2,
+
+            center[0]+width/2,center[1]-height/2,
+            center[0]-width/2,center[1]-height/2
+
+        ];
+        //top-left
+        //top-right
+        //bottom-right
+        //bottom-left
+    },
+    
+    triangle: function (center, size) {
+        return [
+            center[0] - size, center[1] - size,//left
+            center[0], center[1] + size*2,//top
+            center[0] + size, center[1] - size//right
+        ];
+    },
+    
+    triangle2: function (center, size) {
+        let height = SQRT_3 * size;
+        return [
+            center[0] - size, center[1] - (height / 3.0),//left
+            center[0], center[1] + (height * 2.0 / 3.0),//top
+            center[0] + size, center[1] - (height / 3.0)//right
+        ];
+    },
+}
