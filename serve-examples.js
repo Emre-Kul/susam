3ece753f6335bd8996be30cbf9211d6fe8ec850b
Emre Kul <emre.kul@trendyol.com>
2019-03-18 23:40:23 +0300

    heroku deploy improvements

--- serve-examples.js
+++ serve-examples.js
@@ -6,4 +6,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, './examples/')));
 app.use(express.static(path.join(__dirname, './bundle/')));
 
-app.listen(process.env.PORT || 3333, () => {});
+app.listen(process.env.PORT || 3333, () => {
+ console.log("Server Started!");
+});
